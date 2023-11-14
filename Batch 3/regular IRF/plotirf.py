ffb4d6ae9abd852409bcf110cc79eab0a2959761
francescotamburi <79522605+francescotamburi@users.noreply.github.com>
2023-11-14 14:24:42 +0000

    latex conversion, plot IRF

--- Batch 3/regular IRF/plotirf.py
+++ Batch 3/regular IRF/plotirf.py
@@ -1,46 +1,63 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-R = "#e41a1c"
-B = "#377eb8"
-G = "#4daf4a"
-P = "#984ea3"
+C = "#00DDDD"
+M = "#DD00DD"
+Y = "#DDDD00"
+K = "#000000"
 O = "#ff7f00"
-
-s_con = (2*np.sqrt(2*np.log(2)))
-
-a1 = 213.3
-b1 = 0.8
-s1 = b1/s_con
-c1 = 0
-
-a2 = 150.2
-b2 = 0.1
-s2 = b2/s_con
-c2 = -5
-
-a3 = 265.7
-b3 = 0.1
-s3 = b3/s_con
-c3 = 17
+g = "#cdcd"
 
 
+class Gaussian:
+	def __init__(self, fwhm, intensity, shift):
+		self.intensity = intensity
+		self.fwhm = fwhm
+		self.shift = shift
+		self.prep()
+	
+	def prep(self):
+		self.amplitude = self.intensity
+		self.std_dev = self.fwhm/2.355
+		self.mean = self.shift
+		
+	def equation(self, x):
+		return self.amplitude * np.exp(-.5*((x-self.mean)/self.std_dev)**2)
+	
+g1 = Gaussian(213.3, 80, 0)
+g2 = Gaussian(150, 10, -5)
+g3 = Gaussian(267, 10, 17)
 
-x = np.linspace(200,-200,100000)
-y1 = a1 * 1/(s1*np.sqrt(2*np.pi)) * np.exp(-(x-c1)**2/(2*(s1)**2))
-y2 = a2 * 1/(s1*np.sqrt(2*np.pi)) * np.exp(-(x-c2)**2/(2*(s2)**2))
-y3 = a3 * 1/(s1*np.sqrt(2*np.pi)) * np.exp(-(x-c3)**2/(2*(s3)**2))
+x = np.linspace(200,-200,1000000)
+y1 = g1.equation(x)
+y2 = g2.equation(x)
+y3 = g3.equation(x)
 y = y1 + y2 + y3
 
-plt.tick_params(
-    axis='x',          # changes apply to the x-axis
-    which='both',      # both major and minor ticks are affected
-    left=False,      # ticks along the bottom edge are off
-    right=False,         # ticks along the top edge are off
-    labelleft=False) # labels along the bottom edge are off
+def find_peak(y):
+	return index(max(y))
 
-plt.plot(x,y)
-plt.plot(x,y1)
-plt.plot(x,y2)
-plt.plot(x,y3)
+def fwhm(y_list,x):
+	h_index = np.argmax(y_list)
+	h = max(y_list)
+	
+	diff = np.absolute(y_list-h/2)
+	t1 = diff[:h_index].argmin()
+	t2 = diff[h_index:].argmin()
+	print(x[t1],x[t2+h_index])
+	
+	return abs(x[t2+h_index] - x[t1])
+
+print("peak = {}".format(x[np.argmax(y)]))
+print("fwhm = {}".format(fwhm(y, x)))
+
+plt.plot(x,y, color = K, label="IRF")
+plt.plot(x,y1, color = C)
+plt.plot(x,y2, color = M)
+plt.plot(x,y3, color = Y)
+#plt.axhline(y = 50, color = g, linestyle = 'dashed')
+
+plt.ylabel("Intensity %")
+plt.xlabel("Time (ps)")
+plt.legend()
 plt.show()
