0b737a7f529e2440d8b8f6c77bcef8018f429970
francescotamburi <79522605+francescotamburi@users.noreply.github.com>
2023-12-14 11:11:43 +0000

    work

--- Batch 1+2/plotter2.py
+++ Batch 1+2/plotter2.py
@@ -11,7 +11,7 @@ g = "#cdcd"
 
 offset = 1
 
-csv_filename = "a.csv"
+csv_filename = "batch1+2f.csv"
 
 df = pd.read_csv(csv_filename)
 
@@ -23,13 +23,13 @@ tau_8020 = df[df["Sim Int-1"]==80]
 plt.xlabel("Simulated Tau 2 (ps)")
 plt.ylabel("Tau 1 (ps)")
 
-plt.axhline(y = 150, color = g, linestyle = 'dashed')
+plt.axhline(y = 180, color = g, linestyle = 'dashed')
 
 plt.errorbar(tau_2080["Sim Life-2"]-offset, tau_2080["Life-1"]*1000, yerr=tau_2080["Std Life-1"]*1000, linestyle="none", label="20-80", marker = "o", color=B, capsize=5)
 plt.errorbar(tau_5050["Sim Life-2"], tau_5050["Life-1"]*1000, yerr=tau_5050["Std Life-1"]*1000, linestyle="none", label="50-50", marker = "s", color=R, capsize=5)
 plt.errorbar(tau_8020["Sim Life-2"]+offset, tau_8020["Life-1"]*1000, yerr=tau_8020["Std Life-1"]*1000, linestyle="none", label="80-20", marker = "d", color=G, capsize=5)
 
-plt.xlim(185,255)
+plt.legend()
 plt.savefig("t1.png")
 plt.clf()
 
@@ -44,7 +44,6 @@ plt.errorbar(tau_2080["Sim Life-2"]-offset, tau_2080["Life-2"]*1000-tau_2080["Si
 plt.errorbar(tau_5050["Sim Life-2"], tau_5050["Life-2"]*1000-tau_5050["Sim Life-2"], yerr=tau_5050["Std Life-2"]*1000, linestyle="none", label="50-50", marker = "s", color=R, capsize=5)
 plt.errorbar(tau_8020["Sim Life-2"]+offset, tau_8020["Life-2"]*1000-tau_8020["Sim Life-2"], yerr=tau_8020["Std Life-2"]*1000, linestyle="none", label="80-20", marker = "d", color=G, capsize=5)
 
-plt.xlim(185,255)
 plt.legend()
 plt.savefig("t2.png")
 plt.clf()
@@ -60,7 +59,6 @@ plt.axhline(y = 50, color = g, linestyle = 'dashed')
 plt.errorbar(tau_5050["Sim Life-2"], tau_5050["Int-1"], yerr=tau_5050["Std Int-1"], linestyle="none", label="Tau 1", marker="s", color=P, capsize=5)
 plt.errorbar(tau_5050["Sim Life-2"], tau_5050["Int-2"], yerr=tau_5050["Std Int-2"], linestyle="none", label="Tau 2", marker="s", color=O, capsize=5)
 
-plt.xlim(185,255)
 plt.legend()
 plt.savefig("5050.png")
 plt.clf()
@@ -78,7 +76,6 @@ plt.axhline(y = 20, color = g, linestyle = 'dashed')
 plt.errorbar(tau_2080["Sim Life-2"], tau_2080["Int-1"], yerr=tau_2080["Std Int-1"], linestyle="none", label="Tau 1", marker="o", color=P, capsize=5)
 plt.errorbar(tau_2080["Sim Life-2"], tau_2080["Int-2"], yerr=tau_2080["Std Int-2"], linestyle="none", label="Tau 2", marker="o", color=O, capsize=5)
 
-plt.xlim(185,255)
 plt.legend()
 plt.savefig("2080.png")
 plt.clf()
@@ -96,7 +93,6 @@ plt.axhline(y = 20, color = g, linestyle = 'dashed')
 plt.errorbar(tau_8020["Sim Life-2"], tau_8020["Int-1"], yerr=tau_8020["Std Int-1"], linestyle="none", label="Tau 1", marker="d", color=P, capsize=5)
 plt.errorbar(tau_8020["Sim Life-2"], tau_8020["Int-2"], yerr=tau_8020["Std Int-2"], linestyle="none", label="Tau 2", marker="d", color=O, capsize=5)
 
-plt.xlim(185,255)
 plt.legend()
 plt.savefig("8020.png")
 plt.clf()
--- Batch 3/single Gaussian IRF/gauss210/150/output/plotter2.py
+++ Batch 3/single Gaussian IRF/gauss210/150/output/plotter2.py
@@ -20,7 +20,6 @@ tau_5050 = df[df["Sim Int-1"]==50]
 tau_8020 = df[df["Sim Int-1"]==80]
 
 #Tau1
-plt.title("Tau 1 = 150ps")
 plt.xlabel("Simulated Tau 2 (ps)")
 plt.ylabel("Tau 1 (ps)")
 
@@ -29,12 +28,13 @@ plt.axhline(y = 150, color = g, linestyle = 'dashed')
 plt.errorbar(tau_2080["Sim Life-2"]-offset, tau_2080["Life-1"]*1000, yerr=tau_2080["Std Life-1"]*1000, linestyle="none", label="20-80", marker = "o", color=B, capsize=5)
 plt.errorbar(tau_5050["Sim Life-2"], tau_5050["Life-1"]*1000, yerr=tau_5050["Std Life-1"]*1000, linestyle="none", label="50-50", marker = "s", color=R, capsize=5)
 plt.errorbar(tau_8020["Sim Life-2"]+offset, tau_8020["Life-1"]*1000, yerr=tau_8020["Std Life-1"]*1000, linestyle="none", label="80-20", marker = "d", color=G, capsize=5)
-plt.savefig("t1.png")
+
+#plt.xlim(185,235)
+plt.savefig("t1r.png")
 plt.clf()
 
 
 #Tau 2
-plt.title("Tau 2 difference")
 plt.xlabel("Simulated Tau 2 (ps)")
 plt.ylabel("PALSFIT Output - Simulated Tau 2 (ps)")
 
@@ -44,14 +44,14 @@ plt.errorbar(tau_2080["Sim Life-2"]-offset, tau_2080["Life-2"]*1000-tau_2080["Si
 plt.errorbar(tau_5050["Sim Life-2"], tau_5050["Life-2"]*1000-tau_5050["Sim Life-2"], yerr=tau_5050["Std Life-2"]*1000, linestyle="none", label="50-50", marker = "s", color=R, capsize=5)
 plt.errorbar(tau_8020["Sim Life-2"]+offset, tau_8020["Life-2"]*1000-tau_8020["Sim Life-2"], yerr=tau_8020["Std Life-2"]*1000, linestyle="none", label="80-20", marker = "d", color=G, capsize=5)
 
+#plt.xlim(185,235)
 plt.legend()
-plt.savefig("t2.png")
+plt.savefig("t2r.png")
 plt.clf()
 
 #5050
 plt.ylim(0,100)
 
-plt.title("50%-50% Intensity")
 plt.xlabel("Simulated Tau 2 (ps)")
 plt.ylabel("Intensity (%)")
 
@@ -60,15 +60,15 @@ plt.axhline(y = 50, color = g, linestyle = 'dashed')
 plt.errorbar(tau_5050["Sim Life-2"], tau_5050["Int-1"], yerr=tau_5050["Std Int-1"], linestyle="none", label="Tau 1", marker="s", color=P, capsize=5)
 plt.errorbar(tau_5050["Sim Life-2"], tau_5050["Int-2"], yerr=tau_5050["Std Int-2"], linestyle="none", label="Tau 2", marker="s", color=O, capsize=5)
 
+#plt.xlim(185,235)
 plt.legend()
-plt.savefig("5050.png")
+plt.savefig("5050r.png")
 plt.clf()
 
 
 #2080
 plt.ylim(0,100)
 
-plt.title("20%-80% Intensity")
 plt.xlabel("Simulated Tau 2 (ps)")
 plt.ylabel("Intensity (%)")
 
@@ -78,15 +78,15 @@ plt.axhline(y = 20, color = g, linestyle = 'dashed')
 plt.errorbar(tau_2080["Sim Life-2"], tau_2080["Int-1"], yerr=tau_2080["Std Int-1"], linestyle="none", label="Tau 1", marker="o", color=P, capsize=5)
 plt.errorbar(tau_2080["Sim Life-2"], tau_2080["Int-2"], yerr=tau_2080["Std Int-2"], linestyle="none", label="Tau 2", marker="o", color=O, capsize=5)
 
+#plt.xlim(185,235)
 plt.legend()
-plt.savefig("2080.png")
+plt.savefig("2080r.png")
 plt.clf()
 
 
 #8020
 plt.ylim(0,100)
 
-plt.title("80%-20% Intensity")
 plt.xlabel("Simulated Tau 2 (ps)")
 plt.ylabel("Intensity (%)")
 
@@ -96,6 +96,7 @@ plt.axhline(y = 20, color = g, linestyle = 'dashed')
 plt.errorbar(tau_8020["Sim Life-2"], tau_8020["Int-1"], yerr=tau_8020["Std Int-1"], linestyle="none", label="Tau 1", marker="d", color=P, capsize=5)
 plt.errorbar(tau_8020["Sim Life-2"], tau_8020["Int-2"], yerr=tau_8020["Std Int-2"], linestyle="none", label="Tau 2", marker="d", color=O, capsize=5)
 
+#plt.xlim(185,235)
 plt.legend()
-plt.savefig("8020.png")
+plt.savefig("8020r.png")
 plt.clf()
